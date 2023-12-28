b43e8848bac63abc9078e59de46783ba260485ad
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-12-28 13:37:29 +0000

    added axios default.js missing code from walkthrugh to connect back end as a bug came up

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,76 +1,78 @@
- import axios from "axios"
- axios.defaults.baseURL = 'https://thisorthatapi-56bb400a2b0e.herokuapp.com'
+import axios from "axios";
+axios.defaults.baseURL = 'https://thisorthatapi-56bb400a2b0e.herokuapp.com'
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
 
- 
- // Get CSRF token from the csrftoken cookie
- function getCookie(name) {
-   let cookieValue = null;
-   if (document.cookie && document.cookie !== "") {
-     const cookies = document.cookie.split(";");
-     for (let i = 0; i < cookies.length; i++) {
-       const cookie = cookies[i].trim();
-       if (cookie.substring(0, name.length + 1) === name + "=") {
-         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-         break;
-       }
-     }
-   }
-   return cookieValue;
- }
- 
- // Login functionality
- export const login = async (username, password) => {
 
- 
-   try {
-     const response = await axios.post(
-       "/dj-rest-auth/login/",
-       {
-         username,
-         password,
-       },
-       {
-         headers:{
-          "Content-Type":"application/json"
-         }
-       }
-     );
- 
-     return response;
-   } catch (error) {
-     console.log(error);
-     return null;
-   }
- };
- export const signup = async (username, password1, password2) => {
-   console.log("here");
-   try {
-     const response = await axios.post(
-       "/dj-rest-auth/registration/",
-       {
-         username,
-         password1,
-         password2,
-       },
-{
-         headers:{
-          "Content-Type":"application/json"
-         }
-       }
-     );
- 
-     console.log(response);
- 
-     return response;
-   } catch (error) {
-     console.log(error);
-     return null;
-   }
- };
+// Get CSRF token from the csrftoken cookie
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
- export const getAllPost = ()=>{
+// Login functionality
+export const login = async (username, password) => {
 
- }
- export const getOnePost = ()=>{
 
- }
+  try {
+    const response = await axios.post(
+      "/dj-rest-auth/login/",
+      {
+        username,
+        password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+export const signup = async (username, password1, password2) => {
+  console.log("here");
+  try {
+    const response = await axios.post(
+      "/dj-rest-auth/registration/",
+      {
+        username,
+        password1,
+        password2,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    console.log(response);
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getAllPost = () => {
+
+}
+export const getOnePost = () => {
+
+}
